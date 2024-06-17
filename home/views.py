598208f7128b5f6b02656003091f9d7d71dadd9c
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-06-17 13:24:47 +0330

    create HomeView + templates

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class HomeView(View):
+    """
+    Rendering Home page.
+    """
+    
+    def get(self, request):
+        
+        context = {}
+        return render(request, "home/index.html", context)
