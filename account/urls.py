7e553c17dfb9d0dbe32ba23548ff042630209ca7
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-06-18 23:01:56 +0330

    create RegisterView + templates

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,8 @@ from account import views
 urlpatterns = [
     # login path
     path("login/", views.LoginView.as_view(), name="login"),
+    # register path
+    path("register/", views.RegisterView.as_view(), name="register"),
     # logout path
     path("logout/", views.LogoutView.as_view(), name="logout"), 
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth import logout, authenticate, login, get_user_model
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views import View
+
+User = get_user_model()
 
 
 class LoginView(View):
@@ -11,11 +13,10 @@ class LoginView(View):
 
     def get(self, request):
         
-        if request.user.is_authenticated:
-            return redirect("home")
-        else:
+        if not request.user.is_authenticated:
             context = {}
             return render(request, "account/login.html", context)
+        return redirect("home")
         
     def post(self, request):
         
@@ -33,6 +34,54 @@ class LoginView(View):
                 messages.error(request, "Your username or password is invalid")
                 return redirect("login")
         return redirect("home")
+
+
+class RegisterView(View):
+    """
+    This class is for create User Account.
+    """
+    
+    def get(self, request):
+
+        if not request.user.is_authenticated:
+            context = {}
+            return render(request, "account/register.html")
+        else:
+            return redirect("home")
+        
+    def post(self, request):
+        
+        if not request.user.is_authenticated:
+            
+            username = request.POST.get("username")
+            email = request.POST.get("email")
+            password1 = request.POST.get("password1")
+            password2 = request.POST.get("password2")
+            
+            if password1 != password2:
+                messages.error(request, "Your password not match")
+                return redirect("register")
+            
+            try:
+                user = User.objects.create_user(
+                    username=username,
+                    email=email,
+                    password=password1,
+                )
+
+            except:
+                messages.error(request, "The username or password entered is not valid, choose a valid username and strong password")
+                return redirect("register")
+            
+            authenticated_user = authenticate(request, username=username, password=password1)
+                
+            if authenticated_user:
+                login(request, authenticated_user)
+                messages.success(request, "Login successful")
+                return redirect("home")
+            
+        else:
+            return redirect("home")
 
 
 class LogoutView(View):
