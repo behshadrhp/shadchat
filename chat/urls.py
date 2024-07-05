a4d157a8aa96b38ddc3ff24280cefd731c8800fc
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-07-05 16:49:17 +0330

    create PVChatView, templates, urls

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from chat import views
+
 
 urlpatterns = [
-    
+    # chat on pv page
+    path("pv/", views.PVChatView.as_view(), name="pv-chat"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class PVChatView(View):
+    """
+    This class is for rendering chat on pv.
+    """
+    
+    def get(self, request):
+        
+        context = {}
+        return render(request, "chat/pv.html", context)
