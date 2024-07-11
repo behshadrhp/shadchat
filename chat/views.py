d3ddcf048a3bf6d07bac4854669a6166d39b29a4
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-07-11 12:35:26 +0330

    update PVChatView + template

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.db.models import Q
 from django.views import View
+
+from chat.models import Message
 
 User = get_user_model()
 
@@ -12,6 +15,12 @@ class PVChatView(View):
     
     def get(self, request, username):
         person = User.objects.get(username=username)
+        user = request.user
         
-        context = {"person": person}
+        messages = Message.objects.select_related("user").select_related("send_message_to").filter(
+            Q(user=user, send_message_to=person) | 
+            Q(user=person, send_message_to=user)
+        ).order_by('created')
+        
+        context = {"person": person, "messages": messages}
         return render(request, "chat/pv.html", context)
